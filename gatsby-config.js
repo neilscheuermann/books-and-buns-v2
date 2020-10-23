daafa48e206ae4f0592cb21dfe04038ceabcb7f8
Neil Scheuermann <neilscheuermann@gmail.com>
2020-10-23 16:01:23 -0600

    Stripped down to start like Master Gatsby course

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,8 @@
-module.exports = {
+export default {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Books and Buns`,
+    siteUrl: 'http://www.booksandbuns.com',
+    description: 'website for Books and Buns business',
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  plugins: ['gatsby-plugin-styled-components'],
 }
